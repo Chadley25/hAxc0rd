efca4bb69b12902408f9035a161b9eb5e3fe50a6
Bradley <bradleyjc124@gmail.com>
2020-07-27 19:58:09 -0700

    Moved into another directory

--- commands/info/hackerman.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "hackerman",
-    category: "info",
-    description: "",
-    run: async (client, message, args) => {
-        message.reply("https://i.imgur.com/eSqpwUX.gif");
-    }
-}
