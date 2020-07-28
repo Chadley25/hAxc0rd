c3cd9f82809f174557e2900d3eb10a53466523c8
Bradley <bradleyjc124@gmail.com>
2020-07-27 19:56:41 -0700

    Added comments, changed variable types

--- commands/global/seperateMessage.js
+++ commands/global/seperateMessage.js
@@ -4,10 +4,12 @@ module.exports = {
     aliases: "",
     description: "",
     run: async (message, stdout) => {
-        let numOfChars = Math.ceil((stdout.length / 2000));
+        // seperates the string into 2000 character messages
+        // to be compliant with Discord's 2000 character limit
+        const numOfChars = Math.ceil((stdout.length / 2000));
         for (i = 1; i < numOfChars + 1; i++) {
-            let num2 = i * 2000;
-            let num1 = num2 - 2000;
+            const num2 = i * 2000;
+            const num1 = num2 - 2000;
             if (i == numOfChars) {
                 num2 = stdout.length;
             }
