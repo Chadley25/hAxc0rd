ca2b5f92cfec296f6f8428a42d535946e4e702c5
Bradley <bradleyjc124@gmail.com>
2020-07-27 20:01:34 -0700

    Updated embed type, cleaned up, added comments

--- commands/other/cyberchef.js
+++ commands/other/cyberchef.js
@@ -1,38 +1,37 @@
 module.exports = {
     name: "cyberchef",
     category: "other",
-    description: "",
     run: async (client, message, args) => {
         const Discord = require('discord.js');
         const chef = require("cyberchef");
         var arguments = message.toString().slice((message.content).substr(0,message.content.indexOf(' ')).length + 1);
 
-        if (args[0] == "bakeTo") {
+        if (args[0] == "bakeTo") { // converts string to the format requested
             arguments = arguments.substr(7);
             let action = arguments.split(' ')[0];
-            let test = arguments.replace(action, '').substr(1);
+            let string = arguments.replace(action, '').substr(1);
             try {
-                message.reply("here is your result:\n`" + chef.bake(test, "to " + action).toString() + "`");
+                message.reply("here is your result:\n`" + chef.bake(string, "to " + action).toString() + "`");
             } catch (error) {
                 message.channel.send(`**An error has occured:** ${error.message}`);
             }
-        } else if (args[0] == "bakeFrom") {
+        } else if (args[0] == "bakeFrom") { // decodes the string specified
             arguments = arguments.substr(9);
             let action = arguments.split(' ')[0];
-            let test = arguments.replace(action, '').substr(1);
+            let string = arguments.replace(action, '').substr(1);
             try {
-                message.reply("here is your result:\n`" + chef.bake(test, "from " + action).toString() + "`");
+                message.reply("here is your result:\n`" + chef.bake(string, "from " + action).toString() + "`");
             } catch (error) {
                 message.channel.send(`**An error has occured:** ${error.message}`);
             }
         }
-        else if (args[0] == "parseIPv6") {
+        else if (args[0] == "parseIPv6") { // parses through the specified IPv6 address and displays info about it
             try {
                 message.reply("here is your result:\n" + chef.parseIPv6Address(args[1]).toString());
             } catch (error) {
                 message.channel.send(`**An error has occured:** ${error.message}`);
             }
-        } else if (args[0] == "convertTimeZone")  {
+        } else if (args[0] == "convertTimeZone") { // changes the time specified for a different timezone
             let argumentsSplit = arguments.split(' ');
             let date = argumentsSplit[1];
             let time = argumentsSplit[2];
@@ -46,14 +45,14 @@ module.exports = {
                 message.channel.send(`**An error has occured:** ${error.message}`);
             }
         } else {
-            // displays information about the API being used
-            const richembed = new Discord.RichEmbed()
+            // displays information about CyberChef in a message embed
+            const helpEmbed = new Discord.MessageEmbed()
                 .setColor('#f01d0e')
                 .setTitle('CyberChef - Help')
                 .addField('Description', 'CyberChef is a web app/framework for encryption, encoding, compression, and data analysis.')
-                .addField('Arguments', "bake{To/From} {base64, hex, morsecode, kebabcase, charcode, decimal, etc.}\nparseIPv6 {IPv6 address}\nconvertTimeZone {day/month/year hour:minutes:seconds outputTimezone(ex: America/Los_Angeles)}")
+                .addField('Arguments', "bake{To/From} [base64, hex, morsecode, kebabcase, charcode, decimal, etc.] [message]\nparseIPv6 [IPv6 address]\nconvertTimeZone [day/month/year hour:minutes:seconds outputTimezone(ex: America/Los_Angeles)]")
                 .addField('More Information', 'https://gchq.github.io/CyberChef/')
-            message.channel.send(richembed);
+            message.channel.send(helpEmbed);
         }
     }
 }
