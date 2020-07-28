9cf74a2e50149540be27bf5a784fedbd127ed415
Bradley <bradleyjc124@gmail.com>
2020-07-27 19:59:24 -0700

    Changed variable types, updated embed type, & cleaned up

--- commands/osint/dorking.js
+++ commands/osint/dorking.js
@@ -1,24 +1,21 @@
 module.exports = {
     name: "dorking",
     category: "osint",
-    description: "",
     run: async (client, message, args) => {
-        const { exec } = require('child_process');
         const Discord = require('discord.js');
-        let arguments = message.toString().slice((message.content).substr(0,message.content.indexOf(' ')).length + 1);
         
         if (args[0] == "help") {
-            // displays information about the tool being used
-            const richembed = new Discord.RichEmbed()
+            // displays information about google dorking in a message embed
+            const helpEmbed = new Discord.MessageEmbed()
                 .setColor('#f01d0e')
                 .setTitle('Google Dorking - Help')
                 .addField('Description', "Google dorking is a technique that uses Google Search and other Google apps to find security holes in the configuration and computer code that websites use.")
                 .addField('Arguments', "[URL]")
                 .addField('More Information', 'https://en.wikipedia.org/wiki/Google_hacking')    
-            message.channel.send(richembed);
+            message.channel.send(helpEmbed);
         } else {
             const puppeteer = require('puppeteer');
-            let filePath = 'files/' + message.author.tag + '.pdf';
+            const filePath = 'files/' + message.author.tag + '.pdf';
 
             (async () => {
               const browser = await puppeteer.launch();
